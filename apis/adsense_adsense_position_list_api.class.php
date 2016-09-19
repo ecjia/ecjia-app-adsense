5105bd772574410cba996705a6c7473b3aeb2ef7
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-19 19:16:26 +0800

    apis文件中数据库操作方法修改

--- apis/adsense_adsense_position_list_api.class.php
+++ apis/adsense_adsense_position_list_api.class.php
@@ -23,31 +23,29 @@ class adsense_adsense_position_list_api extends Component_Event_Api {
 	 */
 	
 	private function adsense_position_list($options) {
-		$db = RC_Model::model('adsense/ad_position_model');
-		
+		$db = RC_DB::table('ad_position');
 		$filter = array();
 		$filter['keywords']	  = empty($options['keywords']) ? '' : trim($options['keywords']);
 		$filter['page_size']  = empty($options['page_size']) ? 15 : intval($options['page_size']);
 		$filter['current_page'] = empty($options['current_page']) ? 1 : intval($options['current_page']);
-		$filter['position_id']	= empty($options['position_id']) ? '' : $options['position_id'];
+		$filter['position_id']	= empty($options['position_id']) ? 98 : $options['position_id'];
 		$where = array();
 		if (!empty($filter['keywords'])) {
-			$where['position_name'] = array('like' => "%".$filter['keywords']."%");
+			$db->where('position_name', 'like', '%'.mysql_like_quote($filter['keywords']).'%');
 		}
 		if (!empty($filter['position_id'])) {
-			$where['position_id'] = $filter['position_id'];
+			$db->where('position_id', '=', $filter['position_id']);
 		}
 		
 		$limit = null;
-		/* 判断是否需要分页 will.chen*/
+		/* 判断是否需要分页 will.chen*/		
 		if (isset($options['is_page']) && $options['is_page'] == 1) {
-			$count = $db->where($where)->count();
+			$count = $db->count();
 			$page = new ecjia_page($count, $filter['page_size'], 5, '', $filter['current_page']);
 			$filter['record_count'] = $count;
-			$limit = $page->limit();
 		}
 		
-		$result = $db->where($where)->limit($limit)->select();
+		$result = $db->take(15)->skip($page->start_id-1)->get();
 		
 		if (isset($options['is_page']) && $options['is_page'] == 1) {
 			return array('arr' => $result, 'page' => $page->show(15), 'desc' => $page->page_desc());
