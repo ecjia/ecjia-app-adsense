ff4f1b58f8fca533e69f85a01663f6031addc5b7
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-19 16:05:52 +0800

    平台菜单排序更新

--- apis/adsense_admin_menu_api.class.php
+++ apis/adsense_admin_menu_api.class.php
@@ -8,7 +8,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class adsense_admin_menu_api extends Component_Event_Api {
 
     public function call(&$options) {
-        $menus = ecjia_admin::make_admin_menu('07_content', RC_Lang::get('adsense::adsense.ads_manage'), '', 2);
+        $menus = ecjia_admin::make_admin_menu('08_content', RC_Lang::get('adsense::adsense.ads_manage'), '', 8);
         
         $submenus = array(
             ecjia_admin::make_admin_menu('01_adsense_list', RC_Lang::get('adsense::adsense.ads_list'), RC_Uri::url('adsense/admin/init'), 1)->add_purview('adsense_manage'),
