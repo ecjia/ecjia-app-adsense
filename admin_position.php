3501d8af532e98b67d08166292c7007bc48c6116
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-26 11:34:09 +0800

    更新参数

--- admin_position.php
+++ admin_position.php
@@ -104,7 +104,7 @@ class admin_position extends ecjia_admin {
 		$city_id = $citymanage->getCurrentCity(intval($_GET['city_id']));
 		$this->assign('city_id', $city_id);
 		
-		//获取广告列表
+		//获取广告位列表
 		$position = new Ecjia\App\Adsense\PositionManage('adsense', $city_id);
 		$data = $position->getAllPositions();
 		$this->assign('data', $data);
--- templates/admin/adsense_position_list.dwt.php
+++ templates/admin/adsense_position_list.dwt.php
@@ -35,9 +35,9 @@
 		<table class="table table-striped smpl_tbl dataTable table-hide-edit">
 			<thead>
 				<tr>
-				    <th class="w70">{lang key='adsense::adsense.ad_id'}</th>
+				    <th class="w50">{lang key='adsense::adsense.ad_id'}</th>
 	                <th class="w200">{lang key='adsense::adsense.position_name'}</th>
-	                <th class="w150">广告位代号</th>
+	                <th class="w130" data-toggle="sortby" data-sortby="position_code">广告位代号</th>
 	                <th>{lang key='adsense::adsense.position_desc'}</th>
 				    <th class="w100">建议大小</th>
 				    <th class="w100">排序</th>
@@ -57,7 +57,7 @@
 				    </td>
 				    <td><span>{if $val.position_code}{$val.position_code}{else}无{/if}</span></td>
 				    <td><span>{$val.position_desc}</span></td>
-				    <td><span>{$val.ad_width}*{$val.ad_height}</span></td>
+				    <td><span>{$val.ad_width}*{$val.ad_height}px</span></td>
 				    <td><span class="edit_sort cursor_pointer" data-trigger="editable" data-url='{RC_Uri::url("adsense/admin_position/edit_sort", "city_id={$city_id}")}' data-name="sort_order" data-pk="{$val.position_id}" data-title="排序">{$val.sort_order}</span></td>
 				    <td>
 					   	<a class="data-pjax" href='{RC_Uri::url("adsense/admin/init", "position_id={$val.position_id}&city_id={$city_id}")}' title="查看广告"><button class="btn">查看广告</button></a>
