f287466b3bb73bebc39bed6cd8339535d5d24eb5
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-04 15:53:41 +0800

    优化获取广告组内的广告位排序后结果

--- classes/PositionManage.php
+++ classes/PositionManage.php
@@ -173,7 +173,7 @@ class PositionManage
             
             $adPositionsModel = $model->adPositions();
             $adPositionsModel->orderBy('sort_order', 'asc');
-            $adPositionsModel->orderBy('position_id', 'desc');
+            $adPositionsModel->orderBy('position_id', 'asc');
             $result = $adPositionsModel->get(['position_id', 'position_code', 'position_desc', 'max_number']);
             
             $time = \RC_Time::gmtime();
