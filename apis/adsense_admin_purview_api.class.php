cba411f12d59bac7d475e92ae3421f5c96b8f07e
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-05 12:47:22 +0800

    更新样式和权限

--- apis/adsense_admin_purview_api.class.php
+++ apis/adsense_admin_purview_api.class.php
@@ -69,8 +69,9 @@ class adsense_admin_purview_api extends Component_Event_Api {
 			array('action_name' => '轮播图编辑', 'action_code' => 'cycleimage_update', 'relevance' => ''),
 			array('action_name' => '轮播图删除', 'action_code' => 'cycleimage_delete', 'relevance' => ''),
 				
-		
-			
+			array('action_name' => '快捷菜单管理', 'action_code' => 'shortcut_manage', 'relevance' => ''),
+			array('action_name' => '快捷菜单编辑', 'action_code' => 'shortcut_update', 'relevance' => ''),
+			array('action_name' => '快捷菜单删除', 'action_code' => 'shortcut_delete', 'relevance' => ''),
 		);
 		return $purviews;
 	}
