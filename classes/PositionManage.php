d5895bdb95dfff8986543f8f8f65b104ed9682be
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-05 13:50:48 +0800

    更新快捷菜单数据查询

--- classes/PositionManage.php
+++ classes/PositionManage.php
@@ -128,6 +128,33 @@ class PositionManage
 
             return $result->toArray();
         }
+        if ($this->type == 'shortcut') {
+        
+            $repository = new ShortcutMenuRepository();
+            $where = [
+                'position_code' => $code,
+                'city_id'   => $this->city,
+                'type'  => $this->type,
+                ];
+        
+            $model = $repository->findWhereByFirst($where, ['position_id', 'max_number']);
+             
+            if (is_null($model)) {
+                return [];
+            }
+        
+            $adsModel = $model->ads();
+            $adsModel->where('show_client', '&', $client);
+            $adsModel->where('enabled', 1);
+        
+            if ($model->max_number) {
+                $adsModel->take($model->max_number);
+            }
+        
+            $result = $adsModel->get(['ad_id', 'ad_name', 'ad_code', 'ad_link', 'sort_order']);
+        
+            return $result->toArray();
+        }
         elseif ($this->type == 'adsense') {
             $repository = new AdPositionRepository();
             $where = [
