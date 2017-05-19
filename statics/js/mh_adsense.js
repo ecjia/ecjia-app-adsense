7e61e492d1b49bc9fc5b53bf364730c3f35b8a3d
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-19 11:34:08 +0800

    添加广告界面样式

--- statics/js/mh_adsense.js
+++ statics/js/mh_adsense.js
@@ -2,6 +2,17 @@
 ;(function (app, $) {
     app.merchant_adsense_list = {
         init: function () {
+            //搜索功能
+            $("form[name='searchForm'] .search_ad").on('click', function (e) {
+                e.preventDefault();
+                var url = $("form[name='searchForm']").attr('action');
+                var keywords = $("input[name='keywords']").val();
+                if (keywords != '') {
+                    url += '&keywords=' + keywords;
+                }
+                ecjia.pjax(url);
+            });
+ 
             //筛选功能
             $('.screen-btn').on('click', function (e) {
                 e.preventDefault();
@@ -18,7 +29,53 @@
 	    		},
     		});
         }
-    }
+    };
+ 
+    /* **编辑** */
+    app.merchant_adsense_edit = {
+        init: function () {
+        	
+            $(".date").datepicker({
+                format: "yyyy-mm-dd",
+            });
+
+            $("#media_type").change(function () {
+                $(this).children().each(function (i) {
+                    $("#media_type_" + $(this).val()).hide();
+                })
+                $("#media_type_" + $(this).val()).show();
+            });
+
+            app.merchant_adsense_edit.submit_form();
+        },
+ 
+        submit_form: function (formobj) {
+            var $form = $("form[name='theForm']");
+            var option = {
+                rules: {
+                    ad_name: {
+                        required: true
+                    },
+                },
+                messages: {
+                    ad_name: {
+                    	required: "请输入广告名称"
+                    },
+                },
+                submitHandler: function () {
+                    $form.ajaxSubmit({
+                        dataType: "json",
+                        success: function (data) {
+                            ecjia.merchant.showmessage(data);
+                        }
+                    });
+                }
+            }
+            var options = $.extend(ecjia.merchant.defaultOptions.validate, option);
+            $form.validate(options);
+        }
+    };
+ 
 })(ecjia.merchant, jQuery);
  
 // end
