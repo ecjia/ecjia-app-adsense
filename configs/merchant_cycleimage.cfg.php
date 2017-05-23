b154a50765713cb0ca9f00751164c444d28de599
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-23 10:55:31 +0800

    更新商家菜单  和轮播图默认高度

--- configs/merchant_cycleimage.cfg.php
+++ configs/merchant_cycleimage.cfg.php
@@ -5,7 +5,7 @@ return array(
 		'position_name' => '首页轮播图',
 		'position_desc' => '首页轮播图',
 		'ad_width'      => '1000',
-		'ad_height'     => '200',
+		'ad_height'     => '400',
 		'max_number'    => '5',
 		'sort_order' 	=> '1',
 	],	
